53c1b582843716a678d20f5585481f68c6564631
alexandre.bidon <alexandre.bidon.44@gmail.com>
2022-05-09 16:41:18 +0200

    Update server_setup.py

--- server_setup.py
+++ server_setup.py
@@ -43,7 +43,7 @@ class Server():
         @self.app.websocket("/ws")
         async def websocket_endpoint(websocket: WebSocket):
             await self.manager.connect(websocket)
-            await self.manager.send_personal_message(self.pixel_map.return_matrix(), websocket)
+            await self.manager.send_personal_message("TEST", websocket)
             try:
                 while True:
                     data = await websocket.receive_text()
